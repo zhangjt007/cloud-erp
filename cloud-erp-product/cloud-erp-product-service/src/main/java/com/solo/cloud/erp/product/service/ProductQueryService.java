ecfff071f9823194b6a824c09a74cd2e4fa8c577
zhangjt <zhang89jt@163.com>
2018-09-21 14:01:24 +0800

    商品模块配置修改

--- cloud-erp-product/cloud-erp-product-service/src/main/java/com/solo/cloud/erp/product/service/ProductQueryService.java
+++ cloud-erp-product/cloud-erp-product-service/src/main/java/com/solo/cloud/erp/product/service/ProductQueryService.java
@@ -10,11 +10,14 @@ import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "/product/query", tags = "查询类接口")
+@RefreshScope
 @RestController
 public class ProductQueryService implements IErpProductQueryService {
 
@@ -27,6 +30,8 @@ public class ProductQueryService implements IErpProductQueryService {
     private StringRedisTemplate redisTemplate;
     @Autowired
     private IProductQueryManager productQueryManager;
+    @Value("${product.sn}")
+    private String productSn;
 
     @ApiOperation(value = "获取商品详细信息", response = String.class, notes = "根据url传过来的款式编号获取商品详细信息")
     @ApiImplicitParam(name = "productNo", value = "款式编号", required = true, dataType = "String")
@@ -36,7 +41,7 @@ public class ProductQueryService implements IErpProductQueryService {
         mongoLog.info("[商品模块]接口名称：{};开始", "queryByProductNo");
         long startTime = System.currentTimeMillis();
         //Mysql查询
-        ErpProductInfoEntity entity = repository.findByProductNo(productNo);
+        ErpProductInfoEntity entity = repository.findByProductNo(productSn);
         productQueryManager.queryByProductSn(productNo);
         //redis set
         redisTemplate.opsForValue().set(entity.getProductNo(), entity.getProductName());
